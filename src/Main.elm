396a8adf72afbb7d4350fde5203965105d85293c
JorelAli <jorel.k.ali@gmail.com>
2019-11-25 16:52:11 +0000

    Switch to a different markdown library which is more powerful

--- src/Main.elm
+++ src/Main.elm
@@ -7,11 +7,15 @@ import Html exposing (Html, Attribute, div, input, textarea, text, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 
-import Markdown.Elm
-import Markdown.Option
-import Parse exposing (BlockContent)
-import MDInline exposing (..)
-import Tree exposing (Tree)
+import Markdown.Block exposing (..)
+import Markdown.Inline exposing (..)
+--import Markdown.Elm
+--import Markdown.Option
+--import Parse exposing (BlockContent)
+--import MDInline exposing (..)
+--import Tree exposing (Tree)
+
+import Markdown.Block
 
 -- MAIN
 main =
@@ -49,48 +53,92 @@ Package appendices:
 
 generateLatex : String -> String
 generateLatex content = 
-  let latexBody = Parse.toMDBlockTree Markdown.Option.Extended content |> convertMDBlocksToString
+  let latexBody = Markdown.Block.parse Nothing content |> renderBlocks --Parse.toMDBlockTree Markdown.Option.Extended content |> convertMDBlocksToString
       latexHeader = """
 \\documentclass{article}
 \\usepackage{ulem}     %Used for strikethrough
 \\usepackage{hyperref} %Used for hyperlink creation
 
 \\begin{document}
-      """
+"""
       latexFooter = """
 \\end{document}
       """
   in
     latexHeader ++ latexBody ++ latexFooter
 
-convertMDBlocksToString : Tree Parse.MDBlock -> String
-convertMDBlocksToString tree =
-  String.concat (List.map convertMDBlock (Tree.flatten tree))
+renderHeading : Int -> List (Inline i) -> String
+renderHeading depth content =
+  case depth of
+    1 -> "\\section{" ++ renderInlines content ++ "}"
+    2 -> "\\subsection{" ++ renderInlines content ++ "}"
+    3 -> "\\subsubsection{" ++ renderInlines content ++ "}"
+    4 -> "\\subsubsection{" ++ renderInlines content ++ "}" --...
+    _ -> "" --Oops 
 
-convertMDBlock : Parse.MDBlock -> String
-convertMDBlock mdBlock = 
-  case mdBlock of
-    Parse.MDBlock blockType level content -> 
-      case content of
-        Parse.M inline -> convertMDInline inline
-        Parse.T string -> string
+renderBlocks : List (Block b i) -> String
+renderBlocks blocks = List.map renderBlock blocks |> String.concat
 
-convertMDInline : MDInline -> String
-convertMDInline inline =
+renderBlock : Block b i -> String
+renderBlock block = 
+  case block of
+    BlankLine str -> "\n\n"
+    ThematicBreak -> "break"
+    Heading _ level inlines -> renderHeading level inlines
+    CodeBlock _ _ -> "codeblock"
+    Paragraph _ content -> renderInlines content
+    BlockQuote content -> "blockQuote: " ++ (renderBlocks content)
+    List _ _ -> "lst"
+    PlainInlines _ -> "inlines"
+    Markdown.Block.Custom _ _ -> "custom"
+
+renderInlines : List (Inline i) -> String
+renderInlines inlines = List.map renderInline inlines |> String.concat
+
+renderInline : Inline i -> String
+renderInline inline = 
   case inline of
-    OrdinaryText str      -> str
-    ItalicText str        -> "\\textit{" ++ str ++ "}"
-    BoldText str          -> "\\textbf{" ++ str ++ "}"
-    Code str              -> "\\verb|" ++ str ++ "|" --TODO: Handle | characters in code
-    InlineMath str        -> "$" ++ str ++ "$"
-    StrikeThroughText str -> "STRK: " ++ str --"\\sout{" ++ str ++ "}"
-    BracketedText str     -> "[" ++ str ++ "]"
-    Link text url         -> "\\href{" ++ url ++ "}{" ++ text ++ "}"
-    Image url text        -> "img: " ++ url ++ ", " ++ text
-    Line list             -> String.concat (List.map convertMDInline list)
-    Paragraph list        -> "\n\n" ++ String.concat (List.map convertMDInline list)
-    Stanza str            -> "stanza:" ++ str
-    Error list            -> "err"
+    Text str -> str
+    HardLineBreak -> "brk"
+    CodeInline str -> "\\verb|" ++ str ++ "|"
+    Link url _ inlines -> "\\href{" ++ url ++ "}{" ++ renderInlines inlines ++ "}"
+    Image src _ _ -> "img"
+    HtmlInline _ _ _ -> "inlineHTML"
+    Emphasis length content -> 
+      case length of 
+        1 -> "\\textif{" ++ renderInlines content ++ "}" --italics
+        2 -> "\\textbf{" ++ renderInlines content ++ "}" --bold
+        _ -> "wat"
+    Markdown.Inline.Custom _ _ -> "custom"
+
+-- convertMDBlocksToString : Tree Parse.MDBlock -> String
+-- convertMDBlocksToString tree =
+--   String.concat (List.map convertMDBlock (Tree.flatten tree))
+-- 
+-- convertMDBlock : Parse.MDBlock -> String
+-- convertMDBlock mdBlock = 
+--   case mdBlock of
+--     Parse.MDBlock blockType level content -> 
+--       case content of
+--         Parse.M inline -> convertMDInline inline
+--         Parse.T string -> string
+-- 
+-- convertMDInline : MDInline -> String
+-- convertMDInline inline =
+--   case inline of
+--     OrdinaryText str      -> str
+--     ItalicText str        -> "\\textit{" ++ str ++ "}"
+--     BoldText str          -> "\\textbf{" ++ str ++ "}"
+--     Code str              -> "\\verb|" ++ str ++ "|" --TODO: Handle | characters in code
+--     InlineMath str        -> "$" ++ str ++ "$"
+--     StrikeThroughText str -> "STRK: " ++ str --"\\sout{" ++ str ++ "}"
+--     BracketedText str     -> "[" ++ str ++ "]"
+--     Link text url         -> "\\href{" ++ url ++ "}{" ++ text ++ "}"
+--     Image url text        -> "img: " ++ url ++ ", " ++ text
+--     Line list             -> String.concat (List.map convertMDInline list)
+--     Paragraph list        -> "\n\n" ++ String.concat (List.map convertMDInline list)
+--     Stanza str            -> "stanza:" ++ str
+--     Error list            -> "err"
 
 
 
